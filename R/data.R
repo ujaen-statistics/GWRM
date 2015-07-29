edcd9f1d49b427d3f9ea41c94219cee37ccaaac2
ajsaez <ajsaez@ujaen.es>
2015-07-29 13:10:51 +0200

    Metiendo espacios en los items

--- R/data.R
+++ R/data.R
@@ -3,10 +3,10 @@
 #' The response variable \emph{goals}, is the number of goals scored by the footballers (excluding goalkeepers) in the first division of the Spanish league from the 2000/2001 to the 2006/2007 seasons. Since there are footballers who played more than one season, the season in which each one has played more matches has been selected. The covariates considered are the final classification of the team in each season, the position in the field (forward, midfielder and defender) and the number of matches played.
 #' 
 #' \itemize{
-#'   \item{\code{clasif}}{a numeric vector}
-#'   \item{\code{position}}{a factor with levels \code{Defender} \code{Forward} \code{Midfielder}} 
-#'   \item{\code{played}}{a numeric vector}
-#'   \item{\code{goals}}{a numeric vector}
+#'   \item{\code{clasif}} { a numeric vector}
+#'   \item{\code{position}} { a factor with levels \code{Defender} \code{Forward} \code{Midfielder}} 
+#'   \item{\code{played}} { a numeric vector}
+#'   \item{\code{goals}} { a numeric vector}
 #' }
 #' 
 #' @docType data
