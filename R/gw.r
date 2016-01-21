27d880b7bd7670fb185db3f8b2dd41bff380a8ee
SILVERIO VILCHEZ LOPEZ <svilchez@ujaen.es>
2016-01-21 22:05:29 +0100

    Modifico el mensaje para informar de la no convergencia tanto en print.gw como en summary.gw

--- R/gw.r
+++ R/gw.r
@@ -409,9 +409,7 @@ gw.fit <-function (x, y, weights = NULL, k = NULL, kstart = 1, rostart = 2, beta
 #' @importFrom stats coef naprint
 #' @export
 print.gw<-function (x, digits = max(3L, getOption("digits") - 3L), ...) {
-  if(!x$converged){
-    cat("The estimation method does not converge.")
-  }
+
   cat("\nCall:  ", paste(deparse(x$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
   if (length(coef(x))) {
     cat("Coefficients")
@@ -429,15 +427,15 @@ print.gw<-function (x, digits = max(3L, getOption("digits") - 3L), ...) {
   }
   cat("AIC:", format(signif(x$aic, digits)))
   cat("\n")
+  if(!x$converged){
+    cat("Error of convergence")
+  }
   invisible(x)
 }
 
 #' @importFrom stats pnorm
 #' @export
 summary.gw <- function (object, ...){
-  if(!object$converged){
-    cat("The estimation method does not converge.")
-  }
   df.r <- object$df.residual
   if (is.null(object$covars)) object$covars <- "(Intercept)"
   coef.p <- object$betascoefs
@@ -492,7 +490,7 @@ summary.gw <- function (object, ...){
   }
 
   keep <- match(c("call", "terms", "deviance", "aic", "contrasts", "df.residual", "na.action"), names(object), 0L)
-  ans <- c(object[keep], list(coefficients = coef.table, fitted = fitted, betaII = betaII, method = object$method, convergence = object$code))
+  ans <- c(object[keep], list(coefficients = coef.table, fitted = fitted, betaII = betaII, method = object$method, convergence = object$code),converged=object$converged)
   class(ans) <- "summary.gw"
   return(ans)
 }
@@ -526,7 +524,7 @@ print.summary.gw <- function (x, digits = max(3, getOption("digits") - 3), ...){
   }
   else cat("No betaII\n\n")
   cat("\nDegrees of Freedom:", x$df.null, "Total (i.e. Null); ", x$df.residual, "Residual\n")
-  cat("\nCode of convergence:", x$convergence, "\n")
+  cat("\nCode of convergence:", x$convergence, if(!x$converged) "(Error of convergence)" else "", "\n")
   cat("\nMethod:", x$method, "\n")
   if (nzchar(mess <- naprint(x$na.action))){
     cat("  (", mess, ")\n", sep = "")
